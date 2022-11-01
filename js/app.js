9ff4e2f5a7e700bd2ae8ced1851c35dcff443b19
Tyler Thomas <tylert@southern.edu>
2022-10-31 19:09:58 -0500

    added about page, turned off default open on home

--- js/app.js
+++ js/app.js
@@ -3,3 +3,10 @@ const toggleNav = () => {
 	document.body.dataset.nav = document.body.dataset.nav === "on" ? "off" : "on";
 	// document.body.dataset.nav === "off" ? "on" : "off";
 }
+
+// var totalCount = 8;
+// function ChangeIt() {
+// 	var num = Math.ceil( Math.random() * totalCount );
+// 	document.body.background = 'bgimages/'+num+'.jpg';
+// 	document.body.style.backgroundRepeat = "repeat";// Background repeat
+// }
